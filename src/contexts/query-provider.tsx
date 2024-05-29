82b92c5ff17e7d59078f05202961563d38cdab79
Christan Söderberg <christian.v.soderberg@gmail.com>
2024-05-29 18:04:51 +0200

    refactor: update QueryProvider to disable refetch on window focus

--- src/contexts/query-provider.tsx
+++ src/contexts/query-provider.tsx
@@ -1,5 +1,12 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 export function QueryProvider({children}: React.PropsWithChildren) {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
