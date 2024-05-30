ac5b61eb78d1afb3549bcab8f6a159c55d27dcd8
Christan Söderberg <christian.v.soderberg@gmail.com>
2024-05-30 17:33:33 +0200

    feat: add container query for form

--- frontend/components/CandidateForm/CandidateForm.tsx
+++ frontend/components/CandidateForm/CandidateForm.tsx
@@ -1,7 +1,8 @@
 import {useForm} from "react-hook-form";
 import {Candidate, Status} from "../../../types/candidate";
 import {capitalizeFirstLetter} from "../../utils/capitalize";
-import "./CandidateForm.module.css";
+import css from "./CandidateForm.module.css";
+import {useEffect} from "react";
 
 interface CandidateFormProps {
   onSubmit: ((candidate: Candidate) => void) | ((candidate: Omit<Candidate, "id">) => void);
@@ -13,8 +14,13 @@ export function CandidateForm({onSubmit, candidate, onDelete}: CandidateFormProp
   const {
     register,
     handleSubmit,
+    reset,
     formState: {errors},
   } = useForm<Candidate>(candidate ? {defaultValues: candidate} : undefined);
+
+  useEffect(() => {
+    if (!candidate) reset();
+  }, [candidate, reset]);
 
   function handleDelete() {
     if (!candidate || !onDelete) return;
@@ -22,34 +28,34 @@ export function CandidateForm({onSubmit, candidate, onDelete}: CandidateFormProp
   }
 
   return (
-    <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+    <form className={css.form} onSubmit={handleSubmit((data) => onSubmit(data))}>
       <label>
-        Name {errors.name && <span className="error">This field is required</span>}
+        Name {errors.name && <span className={css.error}>This field is required</span>}
         <input type="text" {...register("name", {required: true})} />
       </label>
       <label>
-        Age {errors.age && <span className="error">This field is required</span>}
+        Age {errors.age && <span className={css.error}>This field is required</span>}
         <input type="text" inputMode="numeric" {...register("age", {required: true})} />
       </label>
       <label>
-        Email {errors.email && <span className="error">This field is required</span>}
+        Email {errors.email && <span className={css.error}>This field is required</span>}
         <input type="email" {...register("email", {required: true})} />
       </label>
 
       <label>
-        Street (optional)
+        Street
         <input type="text" {...register("address.street")} />
       </label>
       <label>
-        Postal Code (optional)
+        Postal Code
         <input type="text" {...register("address.postalCode")} />
       </label>
       <label>
-        City (optional)
+        City
         <input type="text" {...register("address.city")} />
       </label>
       <label>
-        Country (optional)
+        Country
         <input type="text" {...register("address.country")} />
       </label>
       <label>
